c8599edfa9f2f20fe98b933bf1169564c5e7b0ee
Daniel Maldonado <damarias991@gmail.com>
2023-06-11 17:30:32 -0500

    11/06: slider gallery configured

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 
 const slider = document.querySelector(".slider");
 const images = document.querySelectorAll(".slider img");
+/*images is a NodeList (It contains elements and we use this NodeList to
+    manipule them: filter, searches, sorting).*/
 let currentIndex = 0;
 
 function nextSlide() {
-  currentIndex = (currentIndex + 1) % images.length; // Calculate the next slide index
+  currentIndex = (currentIndex + 1) % images.length; /*Calculate 
+  the next slide index. It gives numbers from 1 position to final array pos*/
   updateSlider();
+  console.log(currentIndex)
 }
 
 function updateSlider() {
-  const translateXValue = `translateX(-${currentIndex * 100}%)`; // Calculate the translation value
-  slider.style.transform = translateXValue; // Apply the translation to slide to the next image
+  const translateXValue = `translateX(-${currentIndex * 100}%)`;
+  /*This ensures that the slider moves from the right to the left 
+  the 100% of image width.*/
+  slider.style.transform = translateXValue;
+  /* style is for set css property, style.trasform set the x traslation
+  value (100% of the image from right to left).*/ 
 }
 
-setInterval(nextSlide, 5000); // Automatically transition to the next slide every 3 seconds
+setInterval(nextSlide, 3000);
+// Automatically transition to the next slide every 3 seconds
+
